3e8902d5a5b1e417101cf0d4f1ab577531e3d8c4
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-05-26 20:51:38 +0300

    Center column

--- lib/main.dart
+++ lib/main.dart
@@ -9,56 +9,58 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 55.0,
-                backgroundImage: AssetImage('images/me.jpg'),
-              ),
-              Text("kenny mugy",
-                  style: TextStyle(
-                      fontFamily: 'GloriaHallelujah',
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold)),
-              Text("FLUTTER DEV",
-                  style: TextStyle(
-                      fontSize: 20, color: Colors.white, letterSpacing: 4)),
-              Card(
-                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                  child: Padding(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 55.0,
+                  backgroundImage: AssetImage('images/me.jpg'),
+                ),
+                Text("kenny mugy",
+                    style: TextStyle(
+                        fontFamily: 'GloriaHallelujah',
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+                Text("FLUTTER DEV",
+                    style: TextStyle(
+                        fontSize: 20, color: Colors.white, letterSpacing: 4)),
+                Card(
+                    margin: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                            leading: Icon(
+                              Icons.phone,
+                              size: 30,
+                              color: Colors.teal[900],
+                            ),
+                            title: Text("+256 777 705 028",
+                                style: TextStyle(
+                                    color: Colors.teal[900],
+                                    fontFamily: 'GloriaHallelujah',
+                                    fontSize: 20))))),
+                Card(
+                    margin: EdgeInsets.symmetric(horizontal: 40),
+                    child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: ListTile(
-                          leading: Icon(
-                            Icons.phone,
-                            size: 30,
-                            color: Colors.teal[900],
-                          ),
-                          title: Text("+256 777 705 028",
-                              style: TextStyle(
-                                  color: Colors.teal[900],
-                                  fontFamily: 'GloriaHallelujah',
-                                  fontSize: 20))))),
-              Card(
-                  margin: EdgeInsets.symmetric(horizontal: 40),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.teal[900],
+                        leading: Icon(
+                          Icons.email,
+                          color: Colors.teal[900],
+                        ),
+                        title: Text(
+                          "kenmugy@gmail.com",
+                          style: TextStyle(
+                              fontSize: 18,
+                              letterSpacing: 3,
+                              color: Colors.teal[800],
+                              fontFamily: "GloriaHallelujah"),
+                        ),
                       ),
-                      title: Text(
-                        "kenmugy@gmail.com",
-                        style: TextStyle(
-                            fontSize: 18,
-                            letterSpacing: 3,
-                            color: Colors.teal[800],
-                            fontFamily: "GloriaHallelujah"),
-                      ),
-                    ),
-                  ))
-            ],
+                    ))
+              ],
+            ),
           ),
         ),
       ),
